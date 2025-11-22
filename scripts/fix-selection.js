1837ea1695198088761a489d5f30ba2c035f7c0e
PabloEsco12 <val.masu@protonmail.com>
2025-11-22 12:23:08 +0100

    refactor the frontend folder

--- scripts/fix-selection.js
+++
@@ -1,6 +0,0 @@
-﻿const fs=require('fs');
-const path='frontend/secure_messagerie/src/components/MessagesRealtimeNew.vue';
-let text=fs.readFileSync(path,'utf8');
-const regex=/attachmentError\.value = '((?:\\'|[^'])*)'/;
-text=text.replace(regex,"attachmentError.value = 'Sélectionnez une conversation avant d\\'ajouter un fichier.'");
-fs.writeFileSync(path,text,'utf8');
